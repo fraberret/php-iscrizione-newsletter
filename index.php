1c19d05c990c9304b476bbdcec287d91b7c16009
fraberret <f.berretta02@gmail.com>
2024-04-16 16:10:50 +0200

    add markup

---
+++ index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Newsletter</title>
+
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        <header>
+                <nav class="navbar navbar-expand-sm navbar-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Website</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+                            aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarID">
+                            <div class="navbar-nav">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+        </header>
+        <main>
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+            <p class="col-md-8 fs-4">
+                Using a series of utilities, you can create this jumbotron, just
+                like the one in previous versions of Bootstrap. Check out the
+                examples below for how you can remix and restyle it to your liking.
+            </p>
+            <button class="btn btn-primary btn-lg" type="button">
+                Example button
+            </button>
+        </div>
+    </div>
+    
+        </main>
+        <footer class=" py-5 bg-dark text-white">
+      <div class="container text-center">      
+        <form>
+            <h1>Newsletter</h1>
+ 
+ <div class="mb-3 gap-2 d-flex justify-content-center">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control w-25" id="exampleInputEmail1" aria-describedby="emailHelp">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+ </div>
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
